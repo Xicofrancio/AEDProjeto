e7b4a1908f5322a036d85c8ac4696df7fe6deb35
Xicofrancio <franciscodpfsousa@gmail.com>
2022-11-25 01:56:36 +0000

    cont

---
+++ src/Objetos/teste.cpp
@@ -0,0 +1,5 @@
+//
+// Created by USER on 25/11/2022.
+//
+
+#include "teste.h"
---
+++ src/Objetos/teste.h
@@ -0,0 +1,14 @@
+//
+// Created by USER on 25/11/2022.
+//
+
+#ifndef AEDPROJETO_TESTE_H
+#define AEDPROJETO_TESTE_H
+
+
+class teste {
+
+};
+
+
+#endif //AEDPROJETO_TESTE_H
